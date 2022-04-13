45b7bf269b168cea987c26f529610b8861d558d1
DarrenFerns_GIT <fernandesdarren1179@gmail.com>
2022-04-13 13:52:47 -0400

    Background image set and change.

--- app/src/main/java/com/example/recipieapp/MainActivity.java
+++ app/src/main/java/com/example/recipieapp/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getWindow().setBackgroundDrawableResource(R.drawable.bg_image);
         ActionBar actionBar;
         actionBar = getSupportActionBar();
 
